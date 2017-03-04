c81c3ed5aef2428129f6eb4531746dfa60e73944
happy2discover <happy2discover@gmail.com>
2017-03-04 12:33:50 +0800

    add swan

---
+++ Assets/Scripts/SwanMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwanMove : MonoBehaviour {
+	private float moveSpeed = 4; //swan的移动速度
+	// Use this for initialization
+	void Start () {
+		//设置天鹅的初始位置
+		transform.position = new Vector3(22, 3, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x > -22) {
+			//天鹅移动
+			transform.Translate (Vector3.right * -moveSpeed * Time.deltaTime);
+		} else {
+			transform.position = new Vector3 (22, 3, 0);
+		}
+	}
+}
